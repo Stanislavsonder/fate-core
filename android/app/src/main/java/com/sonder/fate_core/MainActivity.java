846c591c262bc148c054b6813a7e3d6808534667
Stanislavsonder <stanislavsonder@gmail.com>
2024-12-23 10:43:44 -0500

    Project renamed. Icons changed

--- android/app/src/main/java/com/sonder/fate_core/MainActivity.java
+++ android/app/src/main/java/com/sonder/fate_core/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.sonder.fate_core;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-	appId: 'io.ionic.starter',
+	appId: 'com.sonder.fate_core',
 	appName: 'FATE: Core',
 	webDir: 'dist'
 }
