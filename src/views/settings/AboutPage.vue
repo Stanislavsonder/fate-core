c4b807d4d92f371b23f4a56f58d9523772f34c84
Stanislavsonder <stanislavsonder@gmail.com>
2024-12-31 19:35:29 -0500

    Source code added

--- src/views/settings/AboutPage.vue
+++ src/views/settings/AboutPage.vue
@@ -1,43 +1,87 @@
 <template>
-		<ion-page>
-			<ion-header>
-				<ion-toolbar>
-					<ion-buttons slot="start">
-						<ion-back-button default-href="/tabs/settings" :text="isIos ? $t('tabs.settings.title') : undefined"/>
-					</ion-buttons>
-					<ion-title class="px-4">{{ $t('settings.about-app.title') }}</ion-title>
-				</ion-toolbar>
-			</ion-header>
-			<ion-content>
-				<ion-list :inset="true">
-					<ion-item>
-						<ion-label>{{ $t('settings.about-app.version')}}</ion-label>
-						<ion-note slot="end" class="text-sm self-center">{{ ABOUT_APP.version }}</ion-note>
-					</ion-item>
-					<ion-item>
-						<ion-label>{{ $t('settings.about-app.author')}}</ion-label>
-						<ion-note slot="end" class="text-sm self-center">
-							{{ ABOUT_APP.author }}
-						</ion-note>
-					</ion-item>
-					<ion-item href="https://opensource.org/license/mit"  :detail="false">
-						<ion-label>{{ $t('settings.about-app.license')}}</ion-label>
-						<ion-note slot="end" class="text-sm self-center flex gap-1 items-center">
-							{{ ABOUT_APP.license}}
-							<ion-icon class="text-lg" :icon="openOutline"/>
-						</ion-note>
-					</ion-item>
-					<ion-item href="https://evilhat.com/product/fate-core-system/"  :detail="false">
-						<ion-label>{{ $t('settings.about-app.original-author')}}</ion-label>
-						<ion-note slot="end" class="text-sm self-center flex gap-1 items-center">
-							{{ ABOUT_APP.evilHat}}
-							<ion-icon class="text-lg" :icon="openOutline"/>
-						</ion-note>
-					</ion-item>
-				</ion-list>
-			</ion-content>
-		</ion-page>
-	</template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-buttons slot="start">
+					<ion-back-button
+						default-href="/tabs/settings"
+						:text="isIos ? $t('tabs.settings.title') : undefined"
+					/>
+				</ion-buttons>
+				<ion-title class="px-4">{{ $t('settings.about-app.title') }}</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<ion-list :inset="true">
+				<ion-item>
+					<ion-label>{{ $t('settings.about-app.version') }}</ion-label>
+					<ion-note
+						slot="end"
+						class="text-sm self-center"
+						>{{ ABOUT_APP.version }}</ion-note
+					>
+				</ion-item>
+				<ion-item>
+					<ion-label>{{ $t('settings.about-app.author') }}</ion-label>
+					<ion-note
+						slot="end"
+						class="text-sm self-center"
+					>
+						{{ ABOUT_APP.author }}
+					</ion-note>
+				</ion-item>
+				<ion-item
+					:href="ABOUT_APP.github.url"
+					:detail="false"
+				>
+					<ion-label>{{ $t('settings.about-app.source-code') }}</ion-label>
+					<ion-note
+						slot="end"
+						class="text-sm self-center flex gap-1 items-center"
+					>
+						{{ ABOUT_APP.github.title }}
+						<ion-icon
+							class="text-lg"
+							:icon="openOutline"
+						/>
+					</ion-note>
+				</ion-item>
+				<ion-item
+					:href="ABOUT_APP.license.url"
+					:detail="false"
+				>
+					<ion-label>{{ $t('settings.about-app.license') }}</ion-label>
+					<ion-note
+						slot="end"
+						class="text-sm self-center flex gap-1 items-center"
+					>
+						{{ ABOUT_APP.license.title }}
+						<ion-icon
+							class="text-lg"
+							:icon="openOutline"
+						/>
+					</ion-note>
+				</ion-item>
+				<ion-item
+					:href="ABOUT_APP.evilHat.url"
+					:detail="false"
+				>
+					<ion-label>{{ $t('settings.about-app.original-author') }}</ion-label>
+					<ion-note
+						slot="end"
+						class="text-sm self-center flex gap-1 items-center"
+					>
+						{{ ABOUT_APP.evilHat.title }}
+						<ion-icon
+							class="text-lg"
+							:icon="openOutline"
+						/>
+					</ion-note>
+				</ion-item>
+			</ion-list>
+		</ion-content>
+	</ion-page>
+</template>
 
 <script setup lang="ts">
 import { IonPage, IonItem, IonNote, IonLabel, IonIcon, IonList, IonContent, IonHeader, IonToolbar, IonBackButton, IonButtons, IonTitle } from '@ionic/vue'
@@ -48,7 +92,17 @@ import { isIos } from '@/utils'
 const ABOUT_APP = {
 	version,
 	author: author.name,
-	license: 'MIT License',
-	evilHat: 'Evil Hat Productions'
+	github: {
+		title: 'GitHub',
+		url: 'https://github.com/Stanislavsonder/fate-core',
+	},
+	license: {
+		title: 'MIT License',
+		url: 'https://opensource.org/license/mit',
+	},
+	evilHat: {
+		title: 'Evil Hat Productions',
+		url: 'https://evilhat.com/product/fate-core-system/',
+	}
 }
 </script>
