622b9049de492ef3715a5639930242a0d23df491
Stanislavsonder <stanislavsonder@gmail.com>
2025-01-13 01:34:31 -0500

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 /// <reference types="vitest" />
-
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite'
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
-
-// @ts-ignore
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
