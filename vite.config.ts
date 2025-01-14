86dd488caebf3f66af2df2e806c42100c97561dd
Stanislavsonder <stanislavsonder@gmail.com>
2025-01-14 18:38:37 -0500

    PWA init commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), legacy(), tailwindcss()],
+	plugins: [vue(), legacy(), tailwindcss(), VitePWA({ registerType: 'autoUpdate', manifest: false })],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src')
