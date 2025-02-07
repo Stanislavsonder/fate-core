8fd518746cdba6d391583b60f09d3093fe442678
Stanislavsonder <stanislavsonder@gmail.com>
2025-02-07 00:32:01 -0500

    fix: status bar issue for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
 	appId: 'com.sonder.fate_core',
 	appName: 'FATE: Core',
-	webDir: 'dist'
+	webDir: 'dist',
+	plugins: {
+		StatusBar: {
+			overlaysWebView: false
+		}
+	}
 }
 
 export default config
